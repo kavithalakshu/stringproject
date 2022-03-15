6586b58b51f7e91b58ccbe08d8f34828125b3513
kavithalakshu <kavisweety4307@gmail.com>
2022-03-15 20:00:10 +0530

    stringproject

---
+++ src/task/Student.java
@@ -0,0 +1,35 @@
+package task;
+
+public class Student {
+public static void main(String[] args) {
+	
+
+	
+	   String s = "welcome";
+	   String s1 = "null ";
+	 
+	
+	
+	   StringBuffer s3 = new StringBuffer("java"); 
+	   System.out.println(System.identityHashCode(s3));
+	   
+	   StringBuilder s5 = new StringBuilder("kjava");
+	   s5.append("kavi");
+	   System.out.println(s5.reverse());
+	   System.out.println(System.identityHashCode(s5));
+	   
+	   
+	   
+}	
+	   
+}	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	
+
